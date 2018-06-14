53a7480c3ee192bf69a2ce9632c0831b808b7e03
imajinyun <imajinyun@gmail.com>
2018-06-14 12:24:29 +0800

    Create catetory model and data

---
+++ database/migrations/2018_06_14_093857_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id')->comment('主键 ID');
+            $table->string('name')->index()->comment('分类名称');
+            $table->string('description')->nullable()->comment('分类描述');
+            $table->integer('post_count')->default(0)->comment('帖子数量');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
