f5f8782b16b74cfb57473ce78baf81e2b138ec70
imajinyun <imajinyun@gmail.com>
2018-06-17 21:50:45 +0800

    Update topic model

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -16,4 +16,14 @@ class Topic extends Model
         'excerpt',
         'slug',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
