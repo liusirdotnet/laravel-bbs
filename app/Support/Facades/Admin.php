2bd4dbd294513bc7e970d7463b3361a789946985
imajinyun <imajinyun@gmail.com>
2018-07-15 16:42:29 +0800

    Add Admin facade class

---
+++ app/Support/Facades/Admin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Support\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static \Illuminate\Foundation\Application|mixed getModel($name)
+ * @method static string getModelClass($name)
+ * @method static void getWidgets()
+ * @method static array getAlerts()
+ * @method static void addFormField()
+ * @method static void getFormField()
+ * @method static string getVersion()
+ */
+class Admin extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return \App\Support\Admin::class;
+    }
+}
