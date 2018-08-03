05668aadac14fd13c3c13e0b23db2813844b5f06
imajinyun <imajinyun@gmail.com>
2018-08-03 11:01:04 +0800

    Update Admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -17,6 +17,8 @@ Route::group(['namespace' => 'Admin', 'as' => 'admin.'], function () {
 
     Route::group(['middleware' => 'admin'], function () {
 
+        $namespacePrefix = '\\' . config('admin.controllers.namespace') . '\\';
+
         // 默认路由。
         Route::get('/', 'DefaultsController@index')->name('dashboard');
 
@@ -26,6 +28,23 @@ Route::group(['namespace' => 'Admin', 'as' => 'admin.'], function () {
         // 用户头像上传路由。
         Route::post('upload', ['UsersController@upload'])->name('users.upload');
 
+        try {
+            foreach (\App\Models\DataType::all() as $dataType) {
+                $breadController = $dataType->controller
+                    ? $dataType->controller
+                    : $namespacePrefix . 'AbstractController';
+
+                Route::get($dataType->slug . '/order', $breadController . '@order')->name($dataType->slug . '.order');
+                Route::post($dataType->slug . '/order', $breadController . '@update_order')->name($dataType->slug
+                    . '.order');
+                Route::resource($dataType->slug, $breadController);
+            }
+        } catch (\InvalidArgumentException $e) {
+            throw new \InvalidArgumentException("Custom routes hasn't been configured because: " . $e->getMessage(), 1);
+        } catch (\Exception $e) {
+            // do nothing, might just be because table not yet migrated.
+        }
+
         // 用户路由。
         Route::resource('users', 'UsersController');
 
@@ -33,8 +52,21 @@ Route::group(['namespace' => 'Admin', 'as' => 'admin.'], function () {
         Route::resource('roles', 'RolesController');
 
         // 菜单路由。
-        Route::group(['prefix' => 'menus'], function () {
-            Route::get('/', 'MenusController@index')->name('menus.index');
+        Route::group([
+            'as'     => 'menus.',
+            'prefix' => 'menus/{menu}',
+        ], function () use ($namespacePrefix) {
+            Route::get('builder', ['uses' => $namespacePrefix . 'MenusController@builder', 'as' => 'builder']);
+            Route::post('order', ['uses' => $namespacePrefix . 'MenusController@order_item', 'as' => 'order']);
+
+            Route::group([
+                'as'     => 'item.',
+                'prefix' => 'item',
+            ], function () use ($namespacePrefix) {
+                Route::delete('{id}', ['uses' => $namespacePrefix . 'MenusController@delete_menu', 'as' => 'destroy']);
+                Route::post('/', ['uses' => $namespacePrefix . 'MenusController@add_item', 'as' => 'add']);
+                Route::put('/', ['uses' => $namespacePrefix . 'MenusController@update_item', 'as' => 'update']);
+            });
         });
     });
 });
