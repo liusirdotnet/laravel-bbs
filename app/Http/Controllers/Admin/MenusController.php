ccfbd8c4755c63f75079b0ff3f461aedeb6be5db
imajinyun <imajinyun@gmail.com>
2018-08-03 11:04:36 +0800

    Update some controller classes

--- app/Http/Controllers/Admin/MenusController.php
+++ app/Http/Controllers/Admin/MenusController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Support\Facades\Admin;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
-class MenusController extends Controller
+class MenusController extends AdminController
 {
-    //
+    public function builder(Request $request, $id)
+    {
+        $menu = Admin::getModel('Menu')->findOrFail($id);
+
+        try {
+            $this->authorize('edit', $menu);
+        } catch (AuthorizationException $e) {
+            //
+        }
+
+        return view('admin.menus.builder', compact([
+            'menu',
+        ]));
+    }
 }
--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -5,134 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
 use App\Support\Facades\Admin;
-use App\Http\Controllers\Admin\Traits\RelationshipParserTrait;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
-class RolesController extends AbstractController
+class RolesController extends AdminController
 {
-    use RelationshipParserTrait;
-
-    public function index(Request $request)
-    {
-        $slug = $this->getSlug($request);
-        $dataType = Admin::getModel('DataType')
-            ->where('slug', '=', $slug)
-            ->first();
-
-        try {
-            $this->authorize('access', app($dataType->model_name));
-        } catch (AuthorizationException $e) {
-            //
-        }
-
-        $getter = 'paginate';
-        $orderBy = strtolower($request->get('order_by'));
-        $orderType = $request->get('order_type');
-        $search = (object) [
-            'key'    => $request->get('key'),
-            'value'  => $request->get('s'),
-            'filter' => $request->get('filter'),
-        ];
-        // $fields = ['ID', '名称', '显示名称', '创建时间', '更新时间',];
-        // $searchable = array_combine(Schema::getColumnListing('roles'), $fields);
-        $searchable = Schema::getColumnListing('roles');
-
-        if ($dataType->model_name !== null) {
-            $relationships = $this->getRelationships($dataType);
-
-            $model = app($dataType->model_name);
-            $query = $model::select('*')->with($relationships);
-
-            $this->removeRelationshipField($dataType, 'access');
-
-            if ($search->key && $search->value && $search->filter) {
-                $filter = $search->filter === 'equals' ? '=' : 'LIKE';
-                $value = $search->filter === 'equals' ? $search->value : '%' . $search->value . '%';
-                $query->where($search->key, $filter, $value);
-            }
-
-            if ($orderBy && \in_array($orderBy, $dataType->fields, true)) {
-                $orderType = $orderType ?: 'DESC';
-                $dataTypeContent = \call_user_func([$query->orderBy($orderBy, $orderType), $getter]);
-            } elseif ($model->timestamps) {
-                $dataTypeContent = \call_user_func([$query->latest($model::CREATED_AT), $getter]);
-            } else {
-                $dataTypeContent = \call_user_func([$query->orderBy($model->getKeyName(), 'DESC')], $getter);
-            }
-            $dataTypeContent = $this->resolveRelations($dataTypeContent, $dataType);
-        } else {
-            // If Model doesn't exist, get data from table name.
-            $dataTypeContent = \call_user_func([\DB::table($dataType->name), $getter]);
-        }
-
-        return view('admin.roles.index', compact(
-            'dataType',
-            'dataTypeContent',
-            'search',
-            'searchable',
-            'orderBy',
-            'orderType'
-        ));
-    }
-
-    public function show(Request $request, $id)
-    {
-        $slug = $this->getSlug($request);
-        $dataType = Admin::getModel('DataType')
-            ->where('slug', '=', $slug)
-            ->first();
-
-        $relationship = $this->getRelationships($dataType);
-        $dataTypeContent = $dataType->model_name !== null
-            ? \call_user_func([app($dataType->model_name)->with($relationship), 'findOrFail'], $id)
-            : DB::table($dataType->name)->where('id', $id)->first();
-
-        $dataTypeContent = $this->resolveRelations($dataTypeContent, $dataType);
-        $this->removeRelationshipField($dataType, 'read');
-
-        try {
-            $this->authorize('read', $dataTypeContent);
-        } catch (AuthorizationException $e) {
-            //
-        }
-
-        return view('admin.roles.detail', compact(
-            'dataType',
-            'dataTypeContent'
-        ));
-    }
-
-    public function create(Request $request)
-    {
-        $slug = $this->getSlug($request);
-
-        $dataType = Admin::getModel('DataType')
-            ->where('slug', '=', $slug)
-            ->first();
-
-        try {
-            $this->authorize('add', app($dataType->model_name));
-        } catch (AuthorizationException $e) {
-            //
-        }
-
-        $dataTypeContent = $dataType->model_name !== null
-            ? new $dataType->model_name()
-            : false;
-
-        foreach ($dataType->addRows as $key => $row) {
-            $details = json_decode($row->details);
-            $dataType->addRows[$key]['col_width'] = isset($details->width) ?? 100;
-        }
-        $this->removeRelationshipField($dataType, 'add');
-
-        return view('admin.roles.role', compact(
-            'dataType',
-            'dataTypeContent'
-        ));
-    }
-
     public function store(Request $request)
     {
         $slug = $this->getSlug($request);
@@ -148,7 +23,7 @@ class RolesController extends AbstractController
 
         if (! $request->ajax()) {
             $model = new $dataType->model_name();
-            $result = $this->saveData($request, $slug, $dataType->addRows, $model);
+            $this->saveData($request, $slug, $dataType->addRows, $model);
             $model->permissions()->sync($request->input('permissions', []));
 
             return redirect()
@@ -158,36 +33,6 @@ class RolesController extends AbstractController
                     'alert-type' => 'success',
                 ]);
         }
-    }
-
-    public function edit(Request $request, $id)
-    {
-        $slug = $this->getSlug($request);
-        $dataType = Admin::getModel('DataType')
-            ->where('slug', '=', $slug)
-            ->first();
-
-        $relationships = $this->getRelationships($dataType);
-        $dataTypeContent = $dataType->model_name !== null
-            ? app($dataType->model_name)->with($relationships)->findOrFail($id)
-            : DB::table($dataType->name)->where('id', $id)->first();
-
-        foreach ($dataType->editRows as $key => $row) {
-            $details = json_decode($row->details);
-            $dataType->editRows[$key]['col_width'] = $details->width ?? 100;
-        }
-        $this->removeRelationshipField($dataType, 'edit');
-
-        try {
-            $this->authorize('edit', $dataTypeContent);
-        } catch (AuthorizationException $e) {
-            //
-        }
-
-        return view('admin.roles.role', compact(
-            'dataType',
-            'dataTypeContent'
-        ));
     }
 
     public function update(Request $request, $id)
