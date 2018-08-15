00f88c5e76544f5f5690ef6b6fe659e0b73fbaf2
imajinyun <imajinyun@gmail.com>
2018-08-15 22:29:30 +0800

    Update DataRowsTableSeeder class

--- database/seeds/DataRowsTableSeeder.php
+++ database/seeds/DataRowsTableSeeder.php
@@ -24,12 +24,13 @@ class DataRowsTableSeeder extends Seeder
     {
         $values = [
             'id' => ['number', 'ID', 1, 0, 0, 0, 0, 0, 1, ''],
-            'name' => ['text', '名称', 1, 1, 1, 1, 1, 1, 2, ''],
-            'email' => ['email', '邮箱', 1, 1, 1, 1, 1, 1, 3, ''],
+            'name' => ['text', '名称', 1, 1, 1, 1, 1, 1, 2, '{"model":"App\\\\Models\\\\User","table":"users","type":"belongsTo","column":"role_id","key":"id","pivot_table":"roles","pivot":"0","validation":{"rule":"required|between:3,25|regex:/^[A-Za-z0-9-_]+$/|unique:users,name,{Auth::id()}","messages":{"required":"用户名 不能为空。","between":"用户名 必须介于 3 - 25 个字符之间。","regex":"用户名 只支持英文、数字、中划线和下划线。","unique":"用户名 已被占用，请重新填写。"}}}'],
+            'email' => ['email', '邮箱', 1, 1, 1, 1, 1, 1, 3, '{"model":"App\\\\Models\\\\User","table":"users","type":"belongsTo","column":"role_id","key":"id","pivot_table":"roles","pivot":"0","validation":{"rule":"required|email","messages":{"required":"邮箱 不能为空。","email":"邮箱 格式不正确，请重新填写。"}}}'],
             'password' => ['email', '密码', 1, 0, 0, 1, 1, 0, 4, ''],
             'remember_token' => ['email', '记住我令牌', 0, 0, 0, 0, 0, 0, 5, ''],
             'avatar' => ['image', '头像', 0, 1, 1, 1, 1, 1, 6, ''],
-            'role' => ['relationship', '角色', 0, 1, 1, 1, 1, 0, 7, '{"model":"App\\Models\\Role","table":"roles","type":"belongsTo","column":"role_id","key":"id","label":"display_name","pivot_table":"roles","pivot":"0"}'],
+            'role' => ['relationship', '角色', 0, 1, 1, 1, 1, 0, 7, '{"model":"App\\\\Models\\\\Role","table":"roles","type":"belongsTo","column":"role_id","key":"id","label":"display_name","pivot_table":"roles","pivot":"0","relationship":{"key":"id","label":"name"}}'],
+            'roles' => ['relationship', '角色', 0, 0, 1, 1, 1, 0, 7, '{"model":"App\\\\Models\\\\Role","table":"roles","type":"belongsToMany","column":"id","key":"id","label":"display_name","pivot_table":"user_roles","pivot":"1","taggable":"0"}'],
             'created_at' => ['timestamp', '创建时间', 0, 1, 1, 0, 0, 0, 8, ''],
             'updated_at' => ['timestamp', '更新时间', 0, 0, 0, 0, 0, 0, 9, ''],
         ];
