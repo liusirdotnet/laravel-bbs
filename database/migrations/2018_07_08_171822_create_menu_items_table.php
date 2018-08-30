c17bad0ea0a385eb72d78e2ab72d0b74ab4e7b41
imajinyun <imajinyun@gmail.com>
2018-08-30 22:11:15 +0800

    Update menu items table migration file

--- database/migrations/2018_07_08_171822_create_menu_items_table.php
+++ database/migrations/2018_07_08_171822_create_menu_items_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateMenuItemsTable extends Migration
 {
@@ -27,6 +27,7 @@ class CreateMenuItemsTable extends Migration
             $table->string('title')
                 ->comment('菜单项名称');
             $table->string('url')
+                ->nullable()
                 ->default('')
                 ->comment('菜单项地址');
             $table->string('route')
