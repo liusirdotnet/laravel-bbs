02257e7072f7df62b5e14957373e6cc5b42f37be
imajinyun <imajinyun@gmail.com>
2018-09-11 21:14:25 +0800

    Api rate limit

--- routes/api.php
+++ routes/api.php
@@ -24,12 +24,18 @@ $api->version('v1', [
         return response('this is version 1.');
     });
 
-    // 发送短信验证码。
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function ($api) {
+        // 发送短信验证码。
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
 
-    // 用户注册。
-    $api->post('users', 'UsersController@store')->name('api.users.store');
+        // 用户注册。
+        $api->post('users', 'UsersController@store')->name('api.users.store');
+    });
 });
 
 $api->version('v2', function ($api) {
