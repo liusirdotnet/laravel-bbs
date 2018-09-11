3c0706b0ff29ef7766b32507117527e2b89bb7d7
imajinyun <imajinyun@gmail.com>
2018-09-11 21:57:15 +0800

    Update api route

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,12 @@ $api->version('v1', [
             ->name('api.verificationCodes.store');
 
         // 用户注册。
-        $api->post('users', 'UsersController@store')->name('api.users.store');
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+
+        // 生成图片验证码。
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
     });
 });
 
