3aa2261cc57b42be6be771da7ea620ae193c2209
imajinyun <imajinyun@gmail.com>
2018-09-12 11:21:16 +0800

    Update services file

--- config/services.php
+++ config/services.php
@@ -47,4 +47,10 @@ return [
         ],
     ],
 
+    'weixin' => [
+        'client_id' => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect' => env('WEIXIN_REDIRECT_URI'),
+    ],
+
 ];
