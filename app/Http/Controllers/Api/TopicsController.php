ebb77ba0660cb45578a2b223c876cf7c3a9a9fef
imajinyun <imajinyun@gmail.com>
2018-09-17 16:57:05 +0800

    Update user topics

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -5,10 +5,56 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\ApiController;
 use App\Http\Requests\Api\TopicRequest;
 use App\Models\Topic;
+use App\Models\User;
 use App\Transformers\TopicTransformer;
+use Illuminate\Http\Request;
 
 class TopicsController extends ApiController
 {
+    /**
+     * 话题列表。
+     *
+     * @param \App\Http\Requests\Api\TopicRequest $request
+     * @param \App\Models\Topic $topic
+     *
+     * @return \Dingo\Api\Http\Response
+     */
+    public function index(Request $request, Topic $topic)
+    {
+        $query = $topic->query();
+
+        if ($categoryId = $request->category_id) {
+            $query->where('category_id', $categoryId);
+        }
+
+        switch ($request->order) {
+            case 'recent':
+                $query->createDesc();
+                break;
+            default:
+                $query->updateDesc();
+                break;
+        }
+        $topics = $query->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
+    }
+
+    /**
+     * 用户话题列表。
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\User $user
+     *
+     * @return \Dingo\Api\Http\Reponse
+     */
+    public function userIndex(Request $request, User $user)
+    {
+        $topics = $user->topics()->createDesc()->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
+    }
+
     /**
      * 创建话题。
      *
