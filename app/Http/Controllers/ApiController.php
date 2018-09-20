cbe9ad9d6aca8568c7124f3b69f8225b33a08f7d
imajinyun <imajinyun@gmail.com>
2018-09-20 08:42:59 +0800

    Add locale middleware to kernel and update api base controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,8 +5,14 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller as BaseController;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ApiController extends BaseController
 {
     use Helpers;
+
+    public function errorResponse($statusCode, $message = null, $code = 0)
+    {
+        throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
